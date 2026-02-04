ae5bdacfc7296cc49e82fce8b608c7a18bd19c26
Vullam Sri Sai Pavan <vullamsaipavan123@gmail.com>
2026-02-04 11:09:02 +0530

    Added Main Project Documentation

--- backend/controllers/alertController.js
+++ backend/controllers/alertController.js
@@ -12,4 +12,22 @@ const getAlerts = async (req, res) => {
     }
 };
 
-module.exports = { getAlerts };
+// Acknowledge/Resolve an Alert
+// PUT /api/alerts/:id/ack
+const acknowledgeAlert = async (req, res) => {
+    try {
+        const alert = await Alert.findById(req.params.id);
+
+        if (alert) {
+            alert.status = 'Resolved';
+            const updatedAlert = await alert.save();
+            res.json(updatedAlert);
+        } else {
+            res.status(404).json({ message: 'Alert not found' });
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+module.exports = { getAlerts, acknowledgeAlert };
--- backend/routes/alertRoutes.js
+++ backend/routes/alertRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { getAlerts } = require('../controllers/alertController');
-const { protect } = require('../middleware/authMiddleware');
+const {
+  getAlerts,
+  //deleteAlert,
+  acknowledgeAlert
+} = require('../controllers/alertController');
 
-//Route: GET / (Matches /api/alerts)
-router.get('/', protect, getAlerts);//get all alerts(/api/alerts)
+const { protect, admin } = require('../middleware/authMiddleware');
+
+router.route('/').get(getAlerts);
+
+//router.route('/:id').delete(protect, admin, deleteAlert);
+
+router.route('/:id/ack').put(protect, acknowledgeAlert);
 
 module.exports = router;
