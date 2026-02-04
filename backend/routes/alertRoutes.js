c59c605a958952000ac864e7b32904e651e8142e
Vullam Sri Sai Pavan <vullamsaipavan123@gmail.com>
2026-02-04 11:18:22 +0530

    Added Main Project Documentation

--- backend/routes/alertRoutes.js
+++ backend/routes/alertRoutes.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const router = express.Router();
+
+// Import the controller functions
 const {
   getAlerts,
   deleteAlert,
   acknowledgeAlert
 } = require('../controllers/alertController');
 
+// Import middleware
 const { protect, admin } = require('../middleware/authMiddleware');
 
+// Define the routes
 router.route('/').get(getAlerts);
 
-router.route('/:id').delete(protect, admin, deleteAlert);
+router.route('/:id')
+    .delete(protect, admin, deleteAlert);
 
-router.route('/:id/ack').put(protect, acknowledgeAlert);
+router.route('/:id/ack')
+    .put(protect, acknowledgeAlert);
 
 module.exports = router;
